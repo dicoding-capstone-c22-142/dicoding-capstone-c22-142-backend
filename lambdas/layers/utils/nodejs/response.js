dfea2fef49edbffd7a62ba59f095d3bbd2f86429
Faturrachman Mochammad <dev@faturrachmanmochammad.id>
2022-11-25 13:45:42 +0700

    Change lambda function Runtime

--- lambdas/layers/utils/nodejs/response.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (status, body) {
-    return {
-        statusCode: status,
-        body: JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-};
